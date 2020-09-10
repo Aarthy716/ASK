0a61b578520da86a3084fe2a7cf8e78601aa5ec8
dfengj <25497306+dfengj@users.noreply.github.com>
2020-09-09 21:34:46 -0700

    End to end 2.5 -1

--- src/test/java/definitions/endToend2_5.java
+++ src/test/java/definitions/endToend2_5.java
@@ -1,44 +1,102 @@
 package definitions;
 
 import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static support.TestContext.getDriver;
 
 public class endToend2_5 {
     @And("I select Single-choice option in Question {int} type checkbox")
     public void iSelectSingleChoiceOptionInQuestionTypeCheckbox(int qNum) {
-        getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'Q"+qNum+"')]/../../..//div[contains(text(),'Single-Choice')]")).click();
+        getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'Q" + qNum + "')]/../../..//div[contains(text(),'Single-Choice')]")).click();
     }
 
     @And("I select Multiple-choice option in Question {int} type checkbox")
     public void iSelectMultipleChoiceOptionInQuestionTypeCheckbox(int qNum) {
-        getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'Q"+qNum+"')]/../../..//div[contains(text(),'Multiple-Choice')]")).click();
+        getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'Q" + qNum + "')]/../../..//div[contains(text(),'Multiple-Choice')]")).click();
     }
 
     @And("I enter {string} question text in Question {int} field")
     public void iEnterQuestionTextInQuestionField(String txt, int qNum) {
-        getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'Q"+qNum+"')]/../../..//textarea[@formcontrolname='question']")).sendKeys(txt);
+        getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'Q" + qNum + "')]/../../..//textarea[@formcontrolname='question']")).sendKeys(txt);
     }
 
 
     @And("I enter {string} text in Option{int} field in Question {int} field")
     public void iEnterTextInOptionFieldInQuestionField(String txt, int optNum, int qNum) {
-        getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'Q\"+qNum+\"')]/../../..//textarea[@placeholder='Option "+optNum+"*']")).sendKeys(txt);
+        getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'Q" + qNum + "')]/../../..//textarea[@placeholder='Option " + optNum + "*']")).sendKeys(txt);
     }
 
-    @And("I select option{int} as a correct answer in {string} \"Question {int} checkbox")
+    @And("I select option{int} as a correct answer in {string} Question {int} checkbox")
     public void iSelectOptionAsACorrectAnswerInQuestionCheckbox(int optNum, String qtype, int qNum) {
-        if(qtype.equalsIgnoreCase("single")) {
-        getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'Q\"+qNum+\"')]/../../..//div/mat-radio-group["+optNum+"]/mat-radio-button")).click();
-    }else if(qtype.equalsIgnoreCase("multiple")) {
-            getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'Q\"+qNum+\"')]/../../..//textarea[@placeholder='Option "+optNum+"*']/../../../../../mat-checkbox")).click();
+        if (qtype.equalsIgnoreCase("single")) {
+            getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'Q" + qNum + "')]/../../..//div/mat-radio-group[" + optNum + "]/mat-radio-button")).click();
+        } else if (qtype.equalsIgnoreCase("multiple")) {
+            getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'Q"+ qNum + "')]/../../..//textarea[@placeholder='Option " + optNum + "*']/../../../../../mat-checkbox")).click();
         }
-        }
+    }
 
     @And("I click Add Option button {int} times in Question {int} field")
     public void iClickAddOptionButtonTimesInQuestionField(int num, int qNum) {
-        for (int i = 0; i < num; i++){
-            getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'Q\"+qNum+\"')]/../../..//span[contains(text(),'Add Option')]")).click();
+        for (int i = 0; i < num; i++) {
+            getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'Q" + qNum + "')]/../../..//span[contains(text(),'Add Option')]")).click();
+        }
+    }
+
+    @And("I select {int} {string} in the Users list on Give Assignment page")
+    public void iSelectInTheUsersListOnGiveAssignmentPage(int num, String sname) throws InterruptedException {
+        int i = 0;
+        for ( WebElement elm:getDriver().findElements(By.xpath
+                ("//mat-list-option/div/div[contains(.,'"+ sname +"')]"))) {
+            elm.click();
+            System.out.println("We selected " + i + "times !");
+            if(i < num) i++;
+            else break;
+            Thread.sleep(2000);
+            //mat-list-option/div/div[contains(.,'" + sname + "'])/..//mat-pseudo-checkbox
+        }
+    }
+
+    @And("I again click [Add Question] button")
+    public void iAgainClickAddQuestionButton() {
+        getDriver().findElement(By.xpath("//div[@class='controls ng-star-inserted shift']//span[1]")).click();
+    }
+
+    @And("I select name {string} of Quiz in Quiz to assign select box")
+    public void iSelectNameOfQuizInQuizToAssignSelectBox(String qname) {
+        getDriver().findElement(By.xpath("(//mat-option/span[contains(text(), '" + qname +"')])[1]")).click();
+    }
+
+    @Then("quiz with name {string} appears")
+    public void quizWithNameAppears(String qname){
+         WebElement elm;
+         elm=getDriver().findElement(By.xpath("//div[@class='quizzes']//mat-panel-title[contains(text(),'" + qname + "')]"));
+         assertThat(elm.isDisplayed()).isTrue();
+    }
+
+    @Then("I delete quiz name {string}")
+    public void iDeleteQuizName(String qName) throws InterruptedException {
+        for (WebElement elm:getDriver().findElements(
+                By.xpath("//div[@class='quizzes']//mat-panel-title[contains(text(),'" + qName + "')]/../../..//span[text()='Delete']"))) {
+            getDriver().findElement(
+                    By.xpath("//div[@class='quizzes']//mat-panel-title[contains(text(),'" + qName + "')]")).click();
+            Thread.sleep(5000);
+            elm.click();
+            System.out.println("We open delete control");
+            Thread.sleep(5000);
+            getDriver().findElement(By.xpath("//mat-dialog-container//span[contains(text(),'Delete')]")).click();
+            Thread.sleep(5000);
+        }
+    }
+
+    @Then("Assignment with quiz name {string} appears")
+    public void assignmentWithQuizNameAppears(String qName) {
+        WebElement elm;
+        elm=getDriver().findElement(By.xpath("//span[@class='mat-content']//*[contains(text(),'End to End 2.5')]"));
+        assertThat(elm.isDisplayed()).isTrue();
     }
 }
+
--- src/test/resources/features/endToEnd2_5.feature
+++ src/test/resources/features/endToEnd2_5.feature
@@ -1,24 +1,28 @@
 Feature: End to End 2.5
+
   Scenario: Create Quiz and assign to 5 individual students
     Given I open "login" page
     When I type email "qa.sofi@gmail.com"
     And I type password "12345"
     And I click Sign in button
-    And I wait 3 seconds
-    Then Text "MyNew Name" appears
+    And I wait 5 seconds
+    Then Text "MyNew" appears
     Then Text "TEACHER" appears
     When I click on Quizzes
     And I wait 10 seconds
+    Then I delete quiz name "End to End 2.5"
+    And I wait 3 seconds
     And I click [Create New Quizz] button
     And I wait 3 seconds
     And I enter "End to End 2.5" quiz title on Create Quiz page
     And I click [Add Question] button
     And I select Single-choice option in Question 1 type checkbox
+    And I wait 3 seconds
     And I enter "1234?" question text in Question 1 field
     And I enter "12" text in Option1 field in Question 1 field
     And I enter "56" text in Option2 field in Question 1 field
-    And I select option2 as a correct answer in "single" "Question 1 checkbox
-    Then I click Add Question button
+    And I select option2 as a correct answer in "single" Question 1 checkbox
+    And I again click [Add Question] button
     And I wait 3 seconds
     And I select Multiple-choice option in Question 2 type checkbox
     And I enter "3456" question text in Question 2 field
@@ -26,20 +30,23 @@ Feature: End to End 2.5
     And I enter "56" text in Option2 field in Question 2 field
     And I click Add Option button 1 times in Question 2 field
     And I enter "78" text in Option3 field in Question 2 field
-    And I select option1 as the correct answer in "multiple" Question 2 checkbox
-    And I select option3 as the correct answer in "multiple" Question 2 checkbox
+    And I select option1 as a correct answer in "multiple" Question 2 checkbox
+    And I select option3 as a correct answer in "multiple" Question 2 checkbox
     And I click [Save] button
-    And I wait 10 seconds
+    And I wait 5 seconds
+    Then quiz with name "End to End 2.5" appears
+    And I wait 5 seconds
     And I select Assignments
+    And I wait 15 seconds
     And I click [Create New Assignment] button
-    And I wait 10 seconds
+    And I wait 5 seconds
     And I click on Select Quiz to assign select box
-    And I select "End to End 2.5" name of Quiz in Quiz to assign select box
-    And I select "Student" 1 in the Users list on Give Assignment page
-    And I select "Student" 2 in the Users list on Give Assignment page
-    And I select "Student" 3 in the Users list on Give Assignment page
-    And I select "Student" 4 in the Users list on Give Assignment page
-    And I select "Student" 5 in the Users list on Give Assignment page
+    And I wait 3 seconds
+    And I select name "End to End 2.5" of Quiz in Quiz to assign select box
+    And I select 5 "Student" in the Users list on Give Assignment page
+    And I wait 3 seconds
     And I click [Give Assignment] button
+    And I wait 5 seconds
+  #  Then Assignment with quiz name "End to End 2.5" appears
     And I click [Log Out] button
     And I confirm Log Out on Confirmation popup
